70f06bcea03dfe4589e077feaa77acf829dbbf48
Jônatas <jonatasthome04@gmail.com>
2025-07-07 17:39:06 -0300

    Finished register post

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,16 +5,38 @@ module.exports = class AuthController{
     static login (req,res){
         res.render("auth/login")
     }
-
+    
     static register(req,res){
         res.render("auth/register")
     }
+    
     static async registerPost(req,res){
         const {name,email,password,confirmpassword} = req.body
         if (password != confirmpassword){
-            req.flash("pass", "As senhas estão incorretas. Tente novamente!")
+            req.flash("msg", "As senhas estão incorretas. Tente novamente!")
             res.render("auth/register")
             return
+        }
+        const checkIfUserExists = await User.findOne({where:{email:email}})
+        if(checkIfUserExists){
+            req.flash("msg", "E-mail já cadastrado. Tente novamente!")
+            res.render("auth/register")
+            return
+        }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        const user = {name, email, password: hashedPassword}
+        try {
+           const userCreated = await User.create(user)
+           req.session.userid = userCreated.id
+           req.flash("msg", "Cadastro efetuado com sucesso!")
+           req.session.save(()=>{
+                res.redirect("/")
+           })
+        } 
+        catch (error) {
+            console.log(error)
         }
     }
 }
