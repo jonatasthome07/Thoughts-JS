362840b2b332b94bd2dc3fc2ddfd1096e80c0f0f
Jônatas <jonatasthome04@gmail.com>
2025-07-08 11:43:48 -0300

    Get thoughts from database

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -7,7 +7,15 @@ module.exports = class ToughtsController{
     }
 
     static async dashboard(req,res){
-        res.render("toughts/dashboard")
+        const userId = req.session.userid
+        const user = await User.findOne({where:{id:userId}, include: Tought, plain:true})
+        if(!user){
+            res.redirect("/login")
+        }
+
+        const toughts = user.Toughts.map((result)=> result.dataValues)
+
+        res.render("toughts/dashboard", {toughts})
     }
 
     static async createTought(req,res){
